e6bb95e4cb8bc1c8d7da3997f315a01b7eb4c0c3
Jan Cortiel <jan.cortiel@gmail.com>
2019-04-29 21:50:23 +0200

    Initial Commit

---
+++ MoTi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoTi
+//
+//  Created by Jan Cortiel on 29.04.19.
+//  Copyright © 2019 Cortiel_Lehner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
