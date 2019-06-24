acc4bde270058866d56f09e52be503d79eaa7fdf
Gerald Lehner <gerald.lehner@hotmail.com>
2019-06-24 14:14:24 +0200

    Very important changes to details 2019-06-24

---
+++ MoTi/DetailsViewController/MovieDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MovieDetailsViewController.swift
+//  MoTi
+//
+//  Created by Gerald on 24.06.19.
+//  Copyright © 2019 Cortiel_Lehner. All rights reserved.
+//
+
+import UIKit
+
+
+//hier code rein fuer den details screen
+
+
+class MovieDetailsViewController : UIViewController{
+    
+    var myMovie : Movie? //of type movie
+    
+    
+}
